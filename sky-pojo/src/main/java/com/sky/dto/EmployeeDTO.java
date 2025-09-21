27a6b9d81f9d3908acd933933042fdc4e9e39c38
fang <2627729817@qq.com>
2025-09-21 13:31:07 +0800

    新增员工[v2.5]

--- sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
@@ -1,22 +1,23 @@
 package com.sky.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
 public class EmployeeDTO implements Serializable {
-
+    @ApiModelProperty("员工id")
     private Long id;
-
+    @ApiModelProperty(value = "员工用户名，不可重复")
     private String username;
-
+    @ApiModelProperty("员工姓名，可重复")
     private String name;
-
+    @ApiModelProperty("员工手机号，11位")
     private String phone;
-
+    @ApiModelProperty("员工性别")
     private String sex;
-
+    @ApiModelProperty("员工身份证")
     private String idNumber;
 
 }
