9b382fbcdc80405d0602e4f02c1598956ac414db
fang <2627729817@qq.com>
2025-10-03 19:32:40 +0800

    店铺营业状态

---
+++ sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -0,0 +1,26 @@
+package com.sky.controller.user;
+
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController()
+@RequestMapping("/user/shop")
+@Api(tags = "店铺相关接口")
+@Slf4j
+public class ShopController {
+    @Autowired
+    private RedisTemplate redisTemplate;
+    public static final String KEY = "SHOP_STATUS";
+    @GetMapping("/status")
+    @ApiOperation("获取营业状态")
+    public Result<Integer> setStatus(){
+        Integer  status = (Integer) redisTemplate.opsForValue().get(KEY);
+        log.info("获取营业状态：{}", status);
+        return Result.success(status);
+    }
+}
