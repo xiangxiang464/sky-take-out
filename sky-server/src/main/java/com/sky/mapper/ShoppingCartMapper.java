298d61853c10790beafc0814c9e22c62e595d279
fang <2627729817@qq.com>
2025-10-05 21:45:04 +0800

    用户端和管理端订单相关功能

--- sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -25,4 +25,6 @@ public interface ShoppingCartMapper {
 
     @Update("delete from shopping_cart where user_id = #{userId}")
     void deleteByUserId(Long userId);
+
+    void insertBatch(List<ShoppingCart> shoppingCartList);
 }
--- sky-server/src/main/java/com/sky/service/OrderService.java
+++ sky-server/src/main/java/com/sky/service/OrderService.java
@@ -1,11 +1,12 @@
 package com.sky.service;
 
-import com.sky.dto.OrdersPaymentDTO;
-import com.sky.dto.OrdersSubmitDTO;
-import com.sky.dto.UserLoginDTO;
+import com.sky.dto.*;
 import com.sky.entity.User;
+import com.sky.result.PageResult;
 import com.sky.vo.OrderPaymentVO;
+import com.sky.vo.OrderStatisticsVO;
 import com.sky.vo.OrderSubmitVO;
+import com.sky.vo.OrderVO;
 
 public interface OrderService {
 
@@ -24,4 +25,26 @@ public interface OrderService {
     void paySuccess(String outTradeNo);
 
     OrderPaymentVO paymentSkipWeChatPay(OrdersPaymentDTO ordersPaymentDTO);
+
+    PageResult pageQuery4User(int page, int pageSize, Integer status);
+
+    OrderVO details(Long id);
+
+    void userCancelById(Long id);
+
+    void repetition(Long id);
+
+    PageResult conditionSearch(OrdersPageQueryDTO ordersPageQueryDTO);
+
+    OrderStatisticsVO statistics();
+
+    void confirm(OrdersConfirmDTO ordersConfirmDTO);
+
+    void rejection(OrdersRejectionDTO ordersRejectionDTO);
+
+    void cancel(OrdersCancelDTO ordersCancelDTO);
+
+    void delivery(Long id);
+
+    void complete(Long id);
 }
